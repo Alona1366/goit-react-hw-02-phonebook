e6571285c3d8f9b4c60bdd85adc4a211fbefd807
Alona Ihnatchenko <olena1366@gmail.com>
2023-03-14 00:49:52 +0200

    create contact list styled

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+export const List = styled.ul`
+   margin: 0;
+  padding: 10;
+  display: flex;
+  flex-direction:column;
+  gap: 8px;
+`;
+
+
+export const ListItem  = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+    padding: 5px;
+  margin-bottom: 8px;
+`;

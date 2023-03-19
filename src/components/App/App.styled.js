fd9193f813200f78a02c56d067bd2c84331b2544
Alona Ihnatchenko <olena1366@gmail.com>
2023-03-19 13:50:42 +0200

    update app styled

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -2,21 +2,21 @@ import styled from '@emotion/styled'
 
 export const Contain = styled.div`
   width: 720px;
-  padding: 35px 15px;
-  margin-top: 20px;
-  border: 3px solid black;
-  border-radius: 4px;
+  padding: 32px 16px;
+  margin-top: 24px;
+  background-color: #eaeae9;
+  border-radius: 8px;
   text-align: center;
   margin-left: auto;
   margin-right: auto;
 `;
 export const PhoneBook = styled.h1`
-  font-size: 29px;
-  color: green;
-  margin-bottom: 25px;
+  font-size: 32px;
+  color: #333333;
+  margin-bottom: 24px;
 `;
 export const PhoneContacts = styled.h1`
-  font-size: 23px;
-  color: green;
-  margin-bottom: 25px;
+  font-size: 24px;
+  color: #333333;
+  margin-bottom: 24px;
 `;
